d1b02f9bdaa731a25d95e543ad3b26266111c07a
Niko Draca <nikola.draca@live.ca>
2017-03-05 00:12:11 -0500

    remove blur function

--- content/blur.js
+++ content/blur.js
@@ -1,23 +1,22 @@
 function addBlur(elem) {
 
 	console.log(elem);
-	elem.style.color = "red";
 
 	var targetDiv = elem;
 
 	if (targetDiv.tagName == "B" || targetDiv.tagName == "I" ) {
 
-		// If it enters here it will stop working
+		// // If it enters here it will stop working
 
-		var targetClone = targetDiv.cloneNode(true);
+		// var targetClone = targetDiv.cloneNode(true);
 
-		var newSpan = document.createElement("span");
-		newSpan.appendChild(targetClone);
-		newSpan.style.display = "inline";
-		newSpan.style.background = "red";
+		// var newSpan = document.createElement("span");
+		// newSpan.appendChild(targetClone);
+		// newSpan.style.display = "inline";
+		// newSpan.style.background = "red";
 
-		targetDiv.parentNode.insertBefore(newSpan, targetDiv);
-		targetDiv.parentNode.removeChild(targetDiv);
+		// targetDiv.parentNode.insertBefore(newSpan, targetDiv);
+		// targetDiv.parentNode.removeChild(targetDiv);
 	}
 
 	else {
@@ -28,8 +27,16 @@ function addBlur(elem) {
 		clearDiv.style.height = (targetDiv.offsetHeight - padding) + "px";
 		clearDiv.style.background = "red";
 		clearDiv.style.position = "absolute";
+		clearDiv.style.zIndex = 10;
+		clearDiv.style.opacity = "0.5";
+		clearDiv.style.filter = "blur(20px);";
+
+		clearDiv.addEventListener("click", removeBlur, false);
 
 		targetDiv.insertBefore(clearDiv, targetDiv.firstChild);
 	}
+}
 
+function removeBlur(){
+	this.parentNode.removeChild(this);
 }
