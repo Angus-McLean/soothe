9d89747c0091677a01dbd326e635e67069f82c6f
Niko Draca <nikola.draca@live.ca>
2017-03-04 20:41:57 -0500

    update chrome permissions
    working

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -5,6 +5,7 @@ for (var i = 0; i < allButtons.length; i++) {
 	allButtons[i].addEventListener("click", toggleTrigger, false);
 };
 
+
 // Reflect colours
 function showActiveTriggers() {
 	chrome.storage.local.get(['activeFilterTypes'], function (arrayOfFilterTypes) {
@@ -13,31 +14,41 @@ function showActiveTriggers() {
 			var button = document.getElementById(item);
 			button.className = "mdl-button mdl-js-button mdl-button--raised mdl-button--accent mdl-color--red-A100";
 		});
+
 	});
 }
 
 // Add new trigger
 function toggleTrigger(){
 
-	var allPermissions = [];
+	window.trigger = this.id;
 
 	chrome.storage.local.get(['activeFilterTypes'], function (arrayOfFilterTypes) {
-		allPermissions = arrayOfFilterTypes.activeFilterTypes;
-		console.log(allPermissions);
+
+		allPermissions = (arrayOfFilterTypes.activeFilterTypes).slice(0);
+
+		updateChromePermissions(allPermissions);
 	});
 
-	if (allPermissions.indexOf(this.id) > -1) {
-		allPermissions.splice(allPermissions.indexOf(this.id, 1));
+	function updateChromePermissions(perms) {
+
+		if (perms.indexOf(trigger) > -1) {
+			console.log("here");
+			perms.splice(perms.indexOf(trigger, 1));
+			document.getElementById(trigger).className = "mdl-button mdl-js-button mdl-button--raised mdl-button--accent mdl-color--light-blue-A100";
+		}
+
+		else {
+			perms.push(trigger);
+		}
+
+		chrome.storage.local.set({'activeFilterTypes': perms});
+		showActiveTriggers();
+		console.log(perms);
+
 	}
 
-	else {
-		allPermissions.push(this.id);
-	}
 
-	chrome.storage.local.set({'activeFilterTypes': allPermissions});
-	showActiveTriggers();
-
-	console.log(allPermissions);
 }
 
 showActiveTriggers();
